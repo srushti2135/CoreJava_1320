03264351fac0cc083cf0c776656c698d6417bbe2
Srushti More <moresrushti0307@gmail.com>
2026-03-09 22:51:39 +0530

    Create ifelse.java

---
+++ Control Flow Statement/If-Else/ifelse.java
@@ -0,0 +1,71 @@
+package ifelse.com;
+
+public class Ifelse {
+	
+	/*public static void main(String args[]) {
+		
+		int num=10;
+		
+		if(num%2==0) {
+			
+			System.out.println("Number is Even : "+num);
+			
+		}else {
+			System.out.println("Number is Odd : "+num);
+		}
+	}
+
+}*/
+	/*public static void main(String args[]) {
+		
+		int age=14;
+		if(age>=18) {
+			
+			System.out.println("Congratulations!!!!!!!!!!! you are eligible for vote");
+		}else {
+			System.out.println("Sorry :( You are not eligible for vote");
+		}
+	
+	}*/
+	
+	/*public static void main(String args[]) {
+		
+		int num=-12;
+		
+		if(num>=0) {
+			
+			System.out.println("Given Number is positive");
+		}else {
+			System.out.println("Given Number is negative");
+		}
+	}*/
+	
+/*public static void main(String args[]) {
+		
+		int num=0;
+		
+		if(num>0) {
+			
+			System.out.println("Given Number is positive");
+		}else if(num==0) {
+			System.out.println("Given Number is neither negative or positive");
+		}else {
+			System.out.println("Given Number is negative");
+		}
+}*/
+	
+public static void main(String args[]) {
+		
+		int num1=100;
+		int num2=10;
+		
+		if(num1>=num2) {
+			
+			System.out.println("Greater number is : "+num1);
+		}else if (num1<=num2){
+			System.out.println("Greater number is : "+num2);
+		}
+}
+	
+	
+	}
