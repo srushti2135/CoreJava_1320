b3d1a2915c96feb8d5c04477ebc18b86873d4500
Srushti More <moresrushti0307@gmail.com>
2026-03-11 13:27:11 +0530

    Create Switch-Case1.java

---
+++ Control Flow Statement/Switch-Case/Switch-Case.java
@@ -0,0 +1,177 @@
+package switchcase;
+
+public class switchcasepractice1 {
+	
+	/*1. Print Day Name
+
+	Write a program to print the day name using a number.
+
+	Example
+	1 → Monday
+	2 → Tuesday
+	
+	public static void main(String args[]) {
+		
+		int day=5;
+		
+		switch(day) {
+		
+		case 1:
+			System.out.println("Monady");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Thursday");
+			break;
+		case 4:
+			System.out.println("Friday");
+			break;
+		case 5:
+			System.out.println("Saturday");
+			break;
+		case 6:
+			System.out.println("Sunday");
+			break;
+			default:
+				System.out.println("Invalid input");
+		}
+	}*/
+	
+	/*2. Print Month Name
+
+Write a program to print month name based on number (1–12).
+
+Example
+1 → January
+2 → February
+
+	
+public static void main(String args[]) {
+		
+		int month=5;
+		
+		switch(month) {
+		
+		case 1:
+			System.out.println("January");
+			break;
+		case 2:
+			System.out.println("February");
+			break;
+		case 3:
+			System.out.println("March");
+			break;
+		case 4:
+			System.out.println("April");
+			break;
+		case 5:
+			System.out.println("May");
+			break;
+		case 6:
+			System.out.println("June");
+			break;
+		case 7:
+			System.out.println("July");
+			break;
+		case 8:
+			System.out.println("Agust");
+			break;
+		case 9:
+			System.out.println("September");
+			break;
+		case 10:
+			System.out.println("October");
+			break;
+		case 11:
+			System.out.println("Noveber");
+			break;
+		case 12:
+			System.out.println("December");
+			break;
+			default:
+				System.out.println("Invalid input");
+		}
+	}*/
+	
+	
+	/*Create a calculator using switch case for operations:
+
+		1 → Addition
+		2 → Subtraction
+		3 → Multiplication
+		4 → Division
+		4. Vowel or Consonant */
+	
+	
+
+	   /* public static void main(String[] args) {
+
+	        int num1 = 10;
+	        int num2 = 5;
+	        int choice = 1;   // 1=Addition, 2=Subtraction, 3=Multiplication, 4=Division
+
+	        switch(choice) {
+
+	            case 1:
+	                System.out.println("Addition = " + (num1 + num2));
+	                break;
+
+	            case 2:
+	                System.out.println("Subtraction = " + (num1 - num2));
+	                break;
+
+	            case 3:
+	                System.out.println("Multiplication = " + (num1 * num2));
+	                break;
+
+	            case 4:
+	                System.out.println("Division = " + (num1 / num2));
+	                break;
+
+	            default:
+	                System.out.println("Invalid choice");
+	        }
+	    
+	}*/
+	
+	
+	  /*  
+	   * Write a program to check whether a character is a vowel or consonant using switch.
+
+Example
+a,e,i,o,u → Vowel
+	   *
+	    public static void main(String[] args) {
+	    
+
+	        char ch = 'a';
+
+	        switch(ch) {
+
+	            case 'a':
+	            case 'e':
+	            case 'i':
+	            case 'o':
+	            case 'u':
+	            case 'A':
+	            case 'E':
+	            case 'I':
+	            case 'O':
+	            case 'U':
+	                System.out.println("It is a Vowel");
+	                break;
+
+	            default:
+	                System.out.println("It is a Consonant");
+	        }
+	    }*/
+	
+	
+	
+	
+	
+	
+
+}
