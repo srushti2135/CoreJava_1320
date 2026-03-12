71d608c9f4c868ec059253204449483605827e79
Srushti More <moresrushti0307@gmail.com>
2026-03-12 22:46:14 +0530

    Create For-loop.java

---
+++ Control Flow Statement/Loops/For-loop.java
@@ -0,0 +1,166 @@
+package loop.com;
+
+public class forloop {
+	
+	//1. Print numbers from 1 to 10 using a loop.
+	
+	/*public static void main(String args[]) {
+		
+		for(int i=1;i<=10;i++) {
+			
+			System.out.println(i);
+			
+			
+		}
+	}*/
+	
+	//2.Print numbers from 10 to 1.
+	
+	/*
+public static void main(String args[]) {
+		
+		for(int i=10;i>=1;i--) {
+			
+			System.out.println(i);
+			
+			
+		}
+	}*/
+	
+	//Print all even numbers between 1 and 50.
+/*	
+public static void main(String args[]) {
+		
+		for(int i=1;i<=50;i++) {
+			if(i%2==0) {
+			System.out.println(i);
+			
+			}
+		}
+	}*/
+
+		//Print all odd numbers between 1 and 50.
+	/*
+public static void main(String args[]) {
+		
+		for(int i=1;i<=50;i++) {
+			if(i%2==	1) {
+			System.out.println(i);
+			
+			}
+		}
+	}*/
+	
+	
+	//Print the table of 5 using a loop.
+	/*
+public static void main(String args[]) {
+		
+		for(int i=1;i<=10;i++) {
+			
+			System.out.println(5*i);
+			
+			
+		}
+	}*/
+	
+	//Print the table of any number (e.g., n = 7).
+	/*
+public static void main(String args[]) {
+		
+		for(int i=1;i<=10;i++) {
+			
+			System.out.println(52*i);
+			
+			
+		}
+	}*/
+	
+	//Print Hello 10 times.
+/*	
+public static void main(String args[]) {
+		
+		for(int i=1;i<=10;i++) {
+			
+			System.out.println("Hello");
+			
+			
+		}
+	}*/
+	
+	//Print the sum of first 10 natural numbers.
+	/*
+public static void main(String args[]) {
+	int sum=0;
+		for(int i=1;i<=10;i++) {
+			
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}*/
+	
+	//Print the sum of even numbers from 1 to 100.
+	
+/*	
+public static void main(String args[]) {
+		int sum=0;
+		for(int i=1;i<=100;i++) {
+			if(i%2==0) {
+				
+				sum=sum+i;
+			
+				
+			}
+		}
+		System.out.println(sum);
+	}
+	*/
+
+//Print the sum of odd numbers from 1 to 100.
+	
+	public static void main(String args[]) {
+		int sum=0;
+		for(int i=1;i<=100;i++) {
+			if(i%2==1) {
+				
+				sum=sum+i;
+			
+				
+			}
+		}
+		System.out.println(sum);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
