d8fe57f31cc702ca5918894c2b649814e1a89541
Srushti More <moresrushti0307@gmail.com>
2026-03-12 22:48:39 +0530

    Add files via upload

---
+++ Control Flow Statement/Loops/dowhile.java
@@ -0,0 +1,96 @@
+package loop.com;
+
+public class dowhile {
+
+	//1. Print numbers from 1 to 10 using a loop.
+		/*
+			public static void main(String args[]) {
+				int i=1;
+				do {
+					
+					System.out.println(i);
+					i++;
+					
+				}while(i<=10);
+			}*/
+	
+	//2.Print numbers from 10 to 1.
+	/*
+	
+		public static void main(String args[]) {
+			int i=10;
+				do {
+					
+					System.out.println(i);
+					i--;
+					
+					
+				}while(i>=1);
+			}*/
+	
+	//Print all even numbers between 1 and 50.
+		
+	/*public static void main(String args[]) {
+		int i=1;
+			do {
+				if(i%2==0) {
+				System.out.println(i);
+				
+				
+				}i++;
+			}while(i<=50);
+		}*/
+	
+	//Print all odd numbers between 1 and 50.
+	
+		
+		public static void main(String args[]) {
+			int i=1;
+				do {
+					if(i%2==1) {
+					System.out.println(i);
+					
+					
+					}i++;
+				}while(i<=50);
+			}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Control Flow Statement/Loops/whileloop.java
@@ -0,0 +1,178 @@
+package loop.com;
+
+public class whileloop {
+	
+	//1. Print numbers from 1 to 10 using a loop.
+	/*
+		public static void main(String args[]) {
+			int i=1;
+			while(i<=10) {
+				
+				System.out.println(i);
+				i++;
+				
+			}
+		}*/
+
+		//2.Print numbers from 10 to 1.
+		
+	
+	/*public static void main(String args[]) {
+		int i=10;
+			while(i>=1) {
+				
+				System.out.println(i);
+				i--;
+				
+				
+			}
+		}*/
+	
+
+	//Print all even numbers between 1 and 50.
+	/*
+public static void main(String args[]) {
+	int i=1;
+		while(i<=50) {
+			if(i%2==0) {
+			System.out.println(i);
+			
+			
+			}i++;
+		}
+	}*/
+	
+	
+	
+	//Print all odd numbers between 1 and 50.
+	
+	/*
+	public static void main(String args[]) {
+		int i=1;
+			while(i<=50) {
+				if(i%2==1) {
+				System.out.println(i);
+				
+				
+				}i++;
+			}
+		}*/
+	
+	//Print the table of 5 using a loop.
+	/*
+public static void main(String args[]) {
+	int i=1;
+		while(i<=10) {
+			
+			System.out.println(5*i);
+			i++;
+			
+		}
+	}
+	*/
+	
+	//Print the table of any number (e.g., n = 7).
+	/*
+	public static void main(String args[]) {
+		int i=1;
+			while(i<=10) {
+				
+				System.out.println(15*i);
+				i++;
+				
+			}
+		}*/
+	
+	//Print Hello 10 times.
+	/*
+	public static void main(String args[]) {
+		int i=1;
+		while(i<=10) {
+			
+			System.out.println("Hello");
+			i++;
+			
+		}
+	}*/
+	
+	//Print the sum of first 10 natural numbers.
+	/*
+	public static void main(String args[]) {
+		int sum=0;
+		int i=1;
+			while(i<=10) {
+				
+				sum=sum+i;
+				i++;
+			}
+			System.out.println(sum);
+		}*/
+	
+	//Print the sum of even numbers from 1 to 100.
+	
+	/*
+	public static void main(String args[]) {
+			int sum=0;
+			int i=1;
+			while(i<=100) {
+				if(i%2==0) {
+					
+					sum=sum+i;
+				
+					
+				}
+				i++;
+			}
+			System.out.println(sum);
+			
+		}
+	*/
+	
+	//Print the sum of odd numbers from 1 to 100.
+/*	
+	public static void main(String args[]) {
+		int sum=0;
+		int i=1;
+		while(i<=100) {
+			if(i%2==1) {
+				
+				sum=sum+i;
+			
+				
+			}
+			i++;
+		}
+		System.out.println(sum);
+		
+	}
+*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
