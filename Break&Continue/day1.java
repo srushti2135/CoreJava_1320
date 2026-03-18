6ffbbc3c8df0ea030c0ac769247307e12fdc9230
Srushti More <moresrushti0307@gmail.com>
2026-03-18 12:57:13 +0530

    Add examples of break and continue statements

---
+++ Break&Continue/day1.java
@@ -0,0 +1,95 @@
+package breakandcontinue;
+
+public class breakcontinue {
+	
+	//Print numbers from 1 to 50 but stop when number becomes 25.
+/*	public static void main(String args[]) {
+		
+		for(int i=1;i<=50;i++) {
+			
+			if(i==25) {
+				break;
+			}
+			System.out.println(i);
+		}
+	}
+*/
+	
+	//Print even numbers from 1 to 100 but stop when you get first odd number.
+	
+	/*
+public static void main(String args[]) {
+		
+		for(int i=1;i<=50;i++) {
+			
+			if(i%2!=0) {
+				break;
+			}
+			System.out.println(i);
+		}
+	}
+	*/
+	
+	//Print table of 9 but stop when product becomes more than 50.
+/*
+public static void main(String args[]) {
+		
+		for(int i=1;i<=10;i++) {
+			 
+			int a=9*i;
+			if(a>=50) {
+				break;
+			}
+			System.out.println(a);
+		}
+	}
+	*/
+	
+	//Print numbers from 1 to 50 but skip numbers divisible by 7.
+	/*
+public static void main(String args[]) {
+		
+		for(int i=1;i<=50;i++) {
+			 
+			
+			if(i%7==0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+	*/
+	
+	//Print numbers from 1 to 100 but skip numbers ending with digit 5.
+/*	
+public static void main(String args[]) {
+		
+		for(int i=1;i<=100;i++) {
+			 
+			
+			if(i%10==5) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+	*/
+	
+	//Print characters from ‘A’ to ‘Z’ but skip vowels.
+	/*
+public static void main(String args[]) {
+		
+		for(char a='A';a<='Z';a++) {
+			 
+			
+			if(a=='A' || a=='E' || a=='I' || a=='O' || a=='U') {
+				continue;
+			}
+			System.out.println(a);
+		}
+	}
+	
+	*/
+	
+	
+}
