3ec08ffca6dbd30b2820e7588b886e7c54ebc973
Srushti More <moresrushti0307@gmail.com>
2026-03-18 12:56:19 +0530

    Create day1.java

---
+++ Star_Pattern/day1.java
@@ -0,0 +1,151 @@
+package starPattern;
+
+public class starpattern {
+	
+	/*
+	*
+	* *
+	* * *
+	* * * * 
+	* * * * *
+	
+	 */
+	
+	/*
+	public static void main(String args[]) {
+		
+		for(int i=1;i<=5;i++) {
+			
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+*/
+	
+	/*
+	 * 
+* * * * *
+* * * *
+* * *
+* *
+*
+
+	 */
+	/*
+public static void main(String args[]) {
+		
+		for(int i=5;i>=1;i--) {
+			
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	*/
+/*	
+	* * * * *
+	* * * * *
+	* * * * *
+	* * * * *
+	* * * * *
+	*/
+	
+	/*
+public static void main(String args[]) {
+		
+		for(int i=1;i<=5;i++) {
+			
+			for(int j=1;j<=5;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	*/
+	
+	/*
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+
+*/
+	
+	/*
+public static void main(String args[]) {
+		
+		for(int i=1;i<=5;i++) {
+			
+			for(int k=1;k<=5-i;k++) {
+				
+				System.out.print(" ");
+			
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print(" *");
+			}
+			System.out.println();
+		
+		}}
+		
+		*/
+	/*
+	    *
+	   * *
+	  * * *
+	   * *
+	    *
+	    
+	    */
+	
+	public static void main(String args[]) {
+		
+		
+			
+			for(int i=1;i<=3;i++) {
+				
+				
+				for(int k=1;k<=3-i;k++) {
+					
+					System.out.print(" ");
+				
+				}
+				for(int j=1;j<=i;j++) {
+					System.out.print(" *");
+				}
+				
+				System.out.println();
+				
+			
+			}
+			
+            for(int i=2;i>=1;i--) {
+				
+				
+				for(int k=1;k<=3-i;k++) {
+					
+					System.out.print(" ");
+				
+				}
+				for(int j=1;j<=i;j++) {
+					System.out.print(" *");
+				}
+				
+				System.out.println();
+				
+			
+			}
+		
+		
+	}
+	
+	
+		}
+	
+	
+
