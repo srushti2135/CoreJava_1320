d7b36bfd5a15c1269985f0ad42e9b48d55029389
Srushti More <moresrushti0307@gmail.com>
2026-03-23 16:19:14 +0530

    Create Prime Number.java

---
+++ Practice Session/Prime Number.java
@@ -0,0 +1,46 @@
+package practice;
+
+public class prime {
+	/* reverse number
+	  
+	 
+	public static void main(String args[]) {
+		
+		int num=88767546;
+		int reverse;
+		while(num!=0) {
+			reverse=num%10;
+			num=num/10;
+			System.out.print(reverse);
+		}
+	}
+*/ 
+	/* Prime number
+	 
+	 
+	public static void main (String args[]) {
+	 int n = 7;
+     boolean isPrime = true;
+
+     if (n <= 1) {
+         isPrime = false;
+     } else {
+
+         for (int i = 2; i <= n / 2; i++) {
+             if (n % i == 0) {
+                 isPrime = false;
+                 break;
+             }
+         }
+     }
+
+     if (isPrime) {
+         System.out.println(n + " is a Prime Number");
+     } else {
+         System.out.println(n + " is NOT a Prime Number");
+ }
+}
+
+*/
+}
+
