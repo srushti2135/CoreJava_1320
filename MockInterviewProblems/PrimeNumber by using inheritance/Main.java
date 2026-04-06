0de4d2dc8b988d768e5b46985c6e1ae9302fd420
Srushti More <moresrushti0307@gmail.com>
2026-04-06 17:28:17 +0530

    Implement main method for prime number checks

---
+++ MockInterviewProblems/PrimeNumber by using inheritance/Main.java
@@ -0,0 +1,19 @@
+package Inheritance;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		
+		Prime p=new Prime();
+		 System.out.println("Prime Number between 1 to 100");
+	    p.isPrime();
+	    
+	   
+	    PrimeNo p1=new PrimeNo();
+	    System.out.println("Given number is prime or not");
+	    p1.isPrime(12);
+	
+	}
+
+}
+
