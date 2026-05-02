a9eb51314bdace7924a0952e4cb9595ed21c8d40
Srushti More <moresrushti0307@gmail.com>
2026-05-02 15:50:28 +0530

    Add files via upload

---
+++ Collection Framework/List.java
@@ -0,0 +1,445 @@
+package CollectionFramework;
+
+import java.util.*;
+
+
+public class List {
+
+	
+	//write a java program to creste an array list & add 10 integers then print them
+//	
+//	public static void main (String args[]) {
+//		
+//		ArrayList<Integer> list = new ArrayList();
+//		list.add(12);
+//		list.add(1562);
+//		list.add(128765);
+//		list.add(12345);
+//		list.add(34512);
+//		list.add(14562);
+//		list.add(17652);
+//		list.add(1234512);
+//		list.add(987612);
+//		list.add(16542);
+//		list.add(14562);
+//		
+//		System.out.println(list);
+//		
+//	}
+	
+	//add elements into vector & print thrm using for each loop
+//	public static void main(String args[]) {
+//		
+//		Vector list= new Vector();
+//		list.add(4567);
+//		list.add(null);
+//		list.add("sdgfhgj");
+//		list.add('f');
+//		list.add(567);
+//		
+//		for(Object obj : list) {
+//			System.out.println(obj);
+//		}
+//		
+//		
+//		
+//	}
+	
+	
+	//create a linked list of strings & print all elements
+	
+//	public static void main(String args[]) {
+//		
+//		LinkedList<String> list = new LinkedList();
+//		list.add("dsdrtyfg");
+//		list.add("iuytr");
+//		list.add("erty");
+//		list.add("fghj");
+//		
+//		System.out.println(list);
+//	}
+	
+	
+	//write a program to finf size of an array list
+
+//	public static void main(String args[]) {
+//		
+//		ArrayList list = new ArrayList();
+//		list.add("gfhh");
+//		list.add(3456);
+//		list.add(45676);
+//		list.add(987654);
+//		list.add("gfhh");
+//		list.add("gfhh");
+//		
+//		System.out.println(list.size());
+//		
+//	}
+	
+	//write a program to check whether element is present in a vector
+	
+	
+
+//	public static void main(String args[]) {
+//		
+//		Vector list= new Vector();
+//		list.add(4567);
+//		list.add(null);
+//		list.add("sdgfhgj");
+//		list.add('f');
+//		list.add(567);
+//		
+//		if(list.contains(567765)) {
+//			System.out.println("Present");
+//			
+//		}
+//		else {
+//			System.out.println("Not Present");
+//		}
+//		
+//	}
+	
+	
+	//write a program to get first & last element from a linked list
+	
+	
+//	public static void main(String args[]) {
+//	
+//	LinkedList<String> list = new LinkedList();
+//	list.add("dsdrtyfg");
+//	list.add("iuytr");
+//	list.add("erty");
+//	list.add("fghj");
+//	
+//	System.out.println(list.getFirst());
+//	System.out.println(list.getLast());
+//	
+//	
+//}
+	
+	//write a program to remove specific element from an array list
+	
+	
+	
+
+//	public static void main(String args[]) {
+//	
+//	ArrayList list = new ArrayList();
+//	list.add("gfhh");
+//	list.add(3456);
+//	list.add(45676);
+//	list.add(987654);
+//	list.add("gfhh");
+//	list.add("gfhh");
+//	System.out.println(list);
+//list.remove(2);
+//	System.out.println("---------------------------------------");
+//	System.out.println(list);
+//	
+//}
+	
+	//write a program to update an element ata specific index in vector
+	
+	
+
+//	public static void main(String args[]) {
+//		
+//		Vector list= new Vector();
+//		list.add(4567);
+//		list.add(null);
+//		list.add("sdgfhgj");
+//		list.add('f');
+//		list.add(567);
+//		
+//		System.out.println(list);
+//		
+//			
+//		list.set(2, "456");
+//		System.out.println("---------------------------------------");
+//		System.out.println(list);
+//		
+//		
+//		
+//		
+//	}
+	
+	//write a program to iterate an arraylist using iterator
+	
+//	public static void main(String a[]) {
+//	ArrayList<String> list = new ArrayList<>();
+//
+//    // Add elements
+//    list.add("Apple");
+//    list.add("Banana");
+//    list.add("Mango");
+//    list.add("Orange");
+//
+// 
+//    Iterator<String> itr = list.iterator();
+//
+//   
+//    System.out.println("ArrayList elements:");
+//    while (itr.hasNext()) {
+//        String element = itr.next();
+//        System.out.println(element);
+//    }
+//}
+	
+	
+	//write a program to reverse linked list
+	
+//	public static void main(String args[]) {
+//		
+//		LinkedList<String> list = new LinkedList();
+//		list.add("dsdrtyfg");
+//		list.add("iuytr");
+//		list.add("erty");
+//		list.add("fghj");
+//		
+//		System.out.println(list.reversed());
+//
+//		
+//		
+//	}
+	
+	//write a program to sort aaraylist in ascending order
+	
+	
+//	public static void main (String args[]) {
+//	
+//	ArrayList list = new ArrayList();
+//	list.add(12);
+//	list.add(1562);
+//	list.add(128765);
+//	list.add(12345);
+//	list.add(34512);
+//	list.add(14562);
+//	list.add(17652);
+//	list.add(1234512);
+//	list.add(987612);
+//	list.add(16542);
+//	list.add(14562);
+//	
+//	Collections.sort(list);
+//	System.out.println(list);
+//	
+//}
+	
+	//copy elements from arraylist to vector
+	
+	
+//	public static void main (String args[]) {
+//		
+//		ArrayList list = new ArrayList();
+//		list.add(12);
+//		list.add(1562);
+//		list.add(128765);
+//		list.add(12345);
+//		list.add(34512);
+//		list.add(14562);
+//		list.add(17652);
+//		list.add(1234512);
+//		list.add(987612);
+//		list.add(16542);
+//		list.add(14562);
+//		
+//		
+//		Vector list1 = new Vector();
+//		list1.addAll(list);
+//		System.out.println(list1);
+//		
+//
+//
+//	}
+	
+	
+	//insert element at specific position in a linked list
+	
+	
+
+//	public static void main(String args[]) {
+//		
+//		LinkedList<String> list = new LinkedList();
+//		list.add("Apple");
+//	    list.add("Banana");
+//	    list.add("Mango");
+//	    list.add("Orange");
+//	    
+//	    System.out.println(list);
+//	    System.out.println("--------------------------");
+//	    list.add(2, "Graphes");
+//	    System.out.println(list);
+//	    
+//
+//
+//	}
+	
+	
+	//remove duplicate elements from arraylist
+	
+//	public static void main(String a[]) {
+//		ArrayList<String> list = new ArrayList<>();
+//	
+//	    list.add("Apple");
+//	    list.add("Banana");
+//	    list.add("Mango");
+//	    list.add("Apple");
+//	    list.add("Orange");
+//	    
+//	    LinkedHashSet<String> set = new LinkedHashSet<>(list);
+//
+//        ArrayList<String> uniqueList = new ArrayList<>(set);
+//	System.out.println(uniqueList);
+//	}
+	
+	//write a program to merge two arraylist objects
+	
+//	
+//	public static void main(String args[]) {
+//		
+//	
+//	ArrayList list = new ArrayList();
+//	list.add(12);
+//	list.add(1562);
+//	list.add(128765);
+//	list.add(12345);
+//	list.add(34512);
+//	
+//	
+//	ArrayList list1 = new ArrayList();
+//	list1.add(14562);
+//	list1.add(17652);
+//	list1.add(1234512);
+//	list1.add(987612);
+//	list1.add(16542);
+//	list1.add(14562);
+//	
+//	list.addAll(list1);
+//	
+//	System.out.println(list);
+//	
+//	
+//	}
+	
+	//convert vector to arraylist
+	
+//	public static void main (String args[]) {
+//	
+//		Vector list= new Vector();
+//		list.add(4567);
+//		list.add(null);
+//		list.add("sdgfhgj");
+//		list.add('f');
+//		list.add(567);
+//		ArrayList list1 = new ArrayList(list);
+//		
+//		System.out.println(list1);
+//		
+//	
+//	}
+	
+	
+	// write a program to search an element in linkedlist & display its position
+	
+//	public static void main(String[] args) {
+//
+//        
+//        LinkedList<Integer> list = new LinkedList<>();
+//
+//        
+//        list.add(10);
+//        list.add(20);
+//        list.add(30);
+//        list.add(40);
+//        list.add(50);
+//
+//        int searchElement = 90;
+//
+//
+//        int index = list.indexOf(searchElement);
+//
+//        if (index != -1) {
+//            System.out.println("Element found at position: " + index);
+//        } else {
+//            System.out.println("Element not found");
+//        }
+//    }
+	
+	//compare two arraylist objects 
+	
+	
+//	public static void main(String args[]) {
+//		
+//	
+//	ArrayList list = new ArrayList();
+//	list.add(12);
+//	list.add(1562);
+//	list.add(128765);
+//	list.add(12345);
+//	list.add(34512);
+//	
+//	
+//	ArrayList list1 = new ArrayList();
+//	list1.add(14562);
+//	list1.add(17652);
+//	list1.add(1234512);
+//	list1.add(987612);
+//	list1.add(16542);
+//	list1.add(14562);
+//	
+//	System.out.println(list.containsAll(list1));
+//		
+//	}
+//	
+	
+	//write a program to counts number of elements in a vector
+	
+	
+//	public static void main (String args[]) {
+//		
+//			Vector list= new Vector();
+//			list.add(4567);
+//			list.add(null);
+//			list.add("sdgfhgj");
+//			list.add('f');
+//			list.add(567);
+//			
+//			System.out.println(list.size());
+//	
+//	
+//	}
+	
+	//write a program to transfer data from arraylist to vector to linked list & print all
+	
+	
+	public static void main(String[] args) {
+
+        
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(10);
+        arrayList.add(20);
+        arrayList.add(30);
+        arrayList.add(40);
+
+        
+        Vector<Integer> vector = new Vector<>(arrayList);
+
+        
+        LinkedList<Integer> linkedList = new LinkedList<>(vector);
+
+        
+        System.out.println("ArrayList: " + arrayList);
+        System.out.println("Vector: " + vector);
+        System.out.println("LinkedList: " + linkedList);
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
